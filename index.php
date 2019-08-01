896ac4adda2e205b67482fcf6e0de43a37f6f5d5
Alex Teterin <at28813905@gmail.com>
2019-08-01 21:13:03 +0300

    убрал пробелы

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
 <?php  
     require_once('inc/common.inc.php');
 
-    
-
-
     const SERVER = "imap.mail.ru";
     const USERNAME = "supermega1@mail.ru";
     const PASSWORD = "1234567";
@@ -11,6 +8,4 @@
     $client = getClient(SERVER, USERNAME, PASSWORD);
     createEventInCalendar($client);
  
-    
-
     echo "hello";
